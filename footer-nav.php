8c32b4e95cf7486aeccd6a7b3f9b92e09f94a6f1
Arl2005 <157169585+Arl2005@users.noreply.github.com>
2024-02-28 11:04:00 -0500

    Add files via upload

---
+++ footer-nav.php
@@ -0,0 +1,9 @@
+<nav>
+	<ul>
+		<li><a href="../week-7-complete/about/privacy-policy.php" title="Read our privacy policy">Privacy Policy</a></li>
+		<li><a href="../week-7-complete/about/privacy-policy.php#personal-information" title="Information regarding personal information">Personal Information</a></li>
+
+		<li><a href="../week-7-complete/about/terms-of-use.php" title="Knowing how to use it"> Terms of Use</a></li>
+		<li><a href="../week-7-complete/about/terms-of-use.php#Setup Information" title="Learn how to use it">How to use it</a></li>
+	</ul>
+</nav>
